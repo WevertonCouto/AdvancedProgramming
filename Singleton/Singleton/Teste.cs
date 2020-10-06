a548a51384304ee06fceb00503b3aa056fa89880
Weverton Couto <wevertoncristais@gmail.com>
2020-10-05 21:32:21 -0300

    Add Singleton

---
+++ Singleton/Singleton/Teste.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Teste
+    {
+        private static Teste teste;
+
+        public static Teste GetInstance()
+        {
+            if (teste == null) teste = new Teste();
+            return teste;
+
+        }
+
+        private Teste()
+        {
+        }
+    }
+}
