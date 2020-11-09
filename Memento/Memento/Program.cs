899629b2f9b15505320fbc34bf77b34aa7facd76
Weverton Couto <wevertoncristais@gmail.com>
2020-11-09 15:25:43 -0300

    Add Memento

---
+++ Memento/Memento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalesProspect s = new SalesProspect();
+            s.Name = "Teste";
+            s.Phone = "(00) 00000-0000";
+            s.Budget = 25000.0;
+
+
+            ProspectMemory m = new ProspectMemory();
+            m.Memento = s.SaveMemento();
+
+            s.Name = "Teste 2";
+            s.Phone = "(00) 0000-0000";
+            s.Budget = 1000000.0;
+
+            s.RestoreMemento(m.Memento);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Memento/Memento/ProspectMemory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    class ProspectMemory
+
+    {
+        private Memento _memento;
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
